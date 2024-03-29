d639ad2d2f66e44c2352b557b7258397d630d092
Xumoyun <129876688+JOKERxtheatre@users.noreply.github.com>
2024-03-29 21:28:23 +0500

    deploying

---
+++ main.js
@@ -0,0 +1,20 @@
+const counters = document.querySelectorAll(".counter");
+counters.forEach((counter) => {
+  counter.textContent = "0";
+
+  function updateCounter() {
+    const target = +counter.getAttribute("data-target");
+    const c = +counter.textContent;
+
+    const increment = target / 200;
+    console.log(increment);
+    if (c < target) {
+      counter.textContent = `${Math.ceil(c+ increment)}`;
+      setTimeout(updateCounter, 0.1)
+    }
+    else {
+        counter.textContent = target
+    }
+  }
+  updateCounter();
+});
